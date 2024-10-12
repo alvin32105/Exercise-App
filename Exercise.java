f118bab10127899f6a88177df6ac6ae908bbff93
Alvin <aat8641@mavs.uta.edu>
2024-10-12 13:03:43 -0500

    getters

--- Exercise.java
+++ Exercise.java
@@ -7,11 +7,31 @@ public class Exercise
     private int sets;
     private int reps;
     
-    public Exercise(exerciseName, pounds, sets, reps)
+    public Exercise(String exerciseName, double pounds, int sets, int reps)
     {
         this.exerciseName = exerciseName;
         this.pounds = pounds;
         this.sets = sets;
         this.reps = reps;
+    }
+
+    public String getName()
+    {
+        return exerciseName;
+    }
+
+    public double getWeight()
+    {
+        return pounds;
+    }
+
+    public int getSets()
+    {
+        return sets;
+    }
+
+    public int getReps()
+    {
+        return reps;
     }
 }
