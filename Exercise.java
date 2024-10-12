b5c8363764832d50a0c20f2aa16580c14a79400a
Ryan Nguyen <rln7471@mavs.uta.edu>
2024-10-12 12:54:53 -0500

    Exercise class

---
+++ Exercise.java
@@ -0,0 +1,17 @@
+
+
+public class Exercise
+{
+    private String exerciseName;
+    private double pounds;
+    private int sets;
+    private int reps;
+    
+    public Exercise(exerciseName, pounds, sets, reps)
+    {
+        this.exerciseName = exerciseName;
+        this.pounds = pounds;
+        this.sets = sets;
+        this.reps = reps;
+    }
+}
